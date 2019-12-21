be959e4fc8f52d6447ddd7bc938ace524165bd3c
Subhash <subhash.kr.lal@gmail.com>
2019-12-21 18:32:08 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import PhoneInput from 'react-native-phone-input';
+import CountryPicker from 'react-native-country-picker-modal';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.onPressFlag = this.onPressFlag.bind(this);
+    this.selectCountry = this.selectCountry.bind(this);
+    this.state = {
+      cca2: 'US',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      pickerData: this.phone.getPickerData(),
+    });
+  }
+
+  onPressFlag() {
+    this.countryPicker.openModal();
+  }
+
+  selectCountry(country) {
+    this.phone.selectCountry(country.cca2.toLowerCase());
+    this.setState({ cca2: country.cca2 });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PhoneInput
+          ref={(ref) => {
+            this.phone = ref;
+          }}
+          onPressFlag={this.onPressFlag}
+        />
+
+        <CountryPicker
+          ref={(ref) => {
+            this.countryPicker = ref;
+          }}
+          onChange={value => this.selectCountry(value)}
+          translation="eng"
+          cca2={this.state.cca2}
+        >
+          <View />
+        </CountryPicker>
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 60,
+  },
+});
+
+module.exports = App;
+
+
+class Example extends Component {  
+  constructor(props){
+    StatusBarIOS.setHidden(true);
+    super(props);
+    this.state = {
+      cca2: 'US'
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Country Picker !
+        </Text>
+        <CountryPicker
+          onChange={(value)=> this.setState({country: value})}
+          cca2={this.state.cca2}
+          translation='eng'
+        />
+        <Text style={styles.instructions}>
+          press on the flag
+        </Text>
+        {this.state.country &&
+          <Text style={styles.data}>
+            {JSON.stringify(this.state.country, null, 2)}
+          </Text>
+        }
+      </View>
+    );
+  }
+}
